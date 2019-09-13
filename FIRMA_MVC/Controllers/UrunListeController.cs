6abe031f2bca0cc1c4abc7d44a506c0764f81075
SALİHA ALTINDİŞ <1305012043@ybu.edu.tr>
2019-09-13 19:39:59 +0300

    13.09.2019 Anasayfa Ekran tasarım ve kodlama(Slider,Bootstrap)

---
+++ FIRMA_MVC/Controllers/UrunListeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FIRMA_MVC.Models;
+
+namespace FIRMA_MVC.Controllers
+{
+    public class UrunListeController : Controller
+    {
+        FIRMAMODEL db = new FIRMAMODEL();
+        // GET: Urunler
+        public ActionResult Index(int? kategoriid)
+        {
+            ViewData["kategori"] = db.KATEGORIs.ToList(); //modelbinding yapmıyoruz kayıt formu olmadığı için.
+
+            if (kategoriid == null)
+            {
+                kategoriid = db.KATEGORIs.ToList()[0].KATEGORI_REFNO;
+            }
+
+            ViewData["urun"] = db.URUNs.Where(s => s.KATEGORI_REFNO == kategoriid).ToList();
+
+            return View();
+        }
+    }
+}
